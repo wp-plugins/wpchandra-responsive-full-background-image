52429611bd3d25fc5d09ea440b802fbcf4aef369
chandgiri67 <chandgiri67@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-25 03:07:36 +0000

    Easy way to add fixed resposive background image to your blog & website. This plugin helps you to easy to add resposive background image.
    git-svn-id: https://plugins.svn.wordpress.org/wpchandra-responsive-full-background-image/trunk@1120135 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0/readme.txt
@@ -0,0 +1,49 @@
+=== WPChandra Responsive Full Background Image ===
+Contributors: chandgiri67
+Tags: wordpress resposnive, responsive, full background, responsive background, background, widget, Post, plugin, admin, posts, sidebar,wpchandra,wpchandra wordpress,chand giri, google, twitter, images, comments, page, shortcode, image
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy way to add fixed resposive background image to your blog & website. This plugin helps you to easy to add resposive background image.
+
+== Description ==
+
+### Responsive Full Background                                                              
+
+Easy way to add fixed resposive background image to your blog & website. This plugin helps you to easy to add resposive background image!
+
+== Screenshots ==
+
+1. Enter your background image url or upload your image.
+2. After all see results like!
+
+### Live Demo 
+
+* [http://www.plugins.wpchandra.com/demo/wpchandra-responsive-full-background-image/](www.plugins.wpchandra.com/demo/wpchandra-responsive-full-background-image/)
+
+### More  Plugins
+
+* [My Plugins](https://profiles.wordpress.org/chandgiri67/#content-plugins)
+
+== Installation ==
+
+* Upload 'wpchandra-responsive-full-background-image' folder to the '/wp-content/plugins/' directory.
+* Activate the plugin through the 'Plugins' menu in WordPress.
+* Go to Appearance menu > Responsive Full Background Image.
+* Enter your background image url or upload your image.
+* Click on 'Save Changes' button.
+* After all injoy this plugin.
+
+== Frequently Asked Questions ==
+* [For questions, contact me.](http://www.wpchandra.com/)
+
+== Changelog ==
+
+= 1.0 =
+WPChandra Support.
+
+= 1.0 =
+First version.
---
+++ 1.0/wp_responsive-full-background-image.php
@@ -0,0 +1,134 @@
+<?php
+/* Plugin Name: WPChandra Responsive Full Background Image
+ Plugin URI: www.plugins.wpchandra.com/demo/wpchandra-responsive-full-background-image/
+ Description: Easy way to add fixed resposive background image to your blog & website. This plugin helps you to easy to add resposive background image.
+ Version: 1.0
+ Author: Chandrakesh Kumar
+ Author URI: http://www.wpchandra.com/
+ License: GPLv2 or later
+ */
+if (!class_exists('WP_Chandra')) {
+	class WP_Chandra {
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct() {
+			add_action('admin_print_scripts', array(&$this, 'my_admin_scripts'));
+            add_action('admin_print_styles', array(&$this, 'my_admin_styles'));
+			add_action('admin_menu', array(&$this, 'wp_add_menu'));
+			add_action( 'wp_head', array(&$this,'wpchandra_responsive_full_background_image') );
+			define(PLUGIN_PATH, WP_PLUGIN_URL.'/wpchandra-responsive-full-background-image/');
+			
+		}// END public function __construct	
+		public function my_admin_scripts() {
+			wp_enqueue_script('media-upload');
+			wp_enqueue_script('thickbox');
+			wp_register_script('wp_upload', PLUGIN_PATH.'js/uploader.js', array('jquery','media-upload','thickbox'));
+			wp_enqueue_script('wp_upload');
+			}
+		public function my_admin_styles() {
+			wp_enqueue_style('thickbox');
+			wp_enqueue_style('admin_settings_style',PLUGIN_PATH.'styles/style.css');
+		}
+		public function register_wp_settings(){
+			register_setting( 'wp_responsive_bg_group', 'image_url' );
+			register_setting( 'wp_responsive_bg_group', 'bg_upload_image' );
+			register_setting( 'wp_responsive_bg_group', 'check_upload_image' );
+		}
+		public function admin_settings_page() {
+
+?>
+<div id="wp_wrap_responsive_bg">
+	<div id="icon-tools" class="icon32"></div>
+	<h2> WPChandra Responsive Full Background Image Settings</h2>
+	<?php if( isset($_GET['settings-updated']) && $_GET['settings-updated'] ){
+	?>
+	<div id="setting-error-settings_updated" class="updated settings-error">
+		<p>
+			<strong>Settings saved.</strong>
+		</p>
+	</div>
+	<?php } ?>
+
+	<form method="post" action="options.php">
+		<?php settings_fields( 'wp_responsive_bg_group' ); ?>
+		<?php $image_url=get_option('image_url'); 
+		if(empty($image_url)){
+			$image_url=PLUGIN_PATH.'images/background-photo.jpg';
+		}
+		
+		?>
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th><label for="image_url">Background Image URL:</label></th>
+					<td>
+					<input class="regular-text" name="image_url" type="text" id="image_url" placeholder="Enter Background Image URL." value="<?php echo $image_url; ?>">
+					</td>
+				</tr>
+				<tr valign="top" >
+					<th scope="row">Upload Image:</th>
+					<td><label for="bg_upload_image">
+						<input id="bg_upload_image" class="regular-text" type="text" size="36" name="bg_upload_image" value="<?php echo get_option('bg_upload_image'); ?>" />
+						<input id="upload_image_button" type="button" value="Upload Image" />
+						<br />
+						<span class="label">Enter an URL or upload an image for the background image.</span> </label></td>
+				</tr>
+				<tr valign="top">
+					<th><label for="image_url"> Checked Option :</label></th>
+					<td>
+					<input  name="check_upload_image" type="checkbox" id="check_upload_image" <?php print(checked(1, get_option('check_upload_image'), false)); ?> value="1"> <label for="check_upload_image">Show upload image for the background image</label> 
+					</td>
+				</tr>
+				<tr valign="top">
+					<td colspan="2"><?php submit_button(); ?></td>
+				</tr>
+				
+			</tbody>
+		</table>
+	</form>
+	<div class="clear"></div>
+</div>
+<?php
+
+}
+
+public function wpchandra_responsive_full_background_image(){
+	$check_upload_image=get_option('check_upload_image');
+	?>
+			<style type="text/css">
+				/* WPChandra Responsive Full Background Image Plugin */
+				html{
+					background-image:none !important;
+					background:none !important;
+				}
+				body{
+					  /* Location of the image */
+					  background-image: url(<?php if($check_upload_image){ echo get_option('bg_upload_image'); } else{ echo get_option('image_url'); } ?> )!important;
+					  /* Image is centered vertically and horizontally at all times */
+					  background-position: center center!important;
+					  /* Image doesn't repeat */
+					  background-repeat: no-repeat!important;
+					  /* Makes the image fixed in the viewport so that it doesn't move when 
+					     the content height is greater than the image height */
+					  background-attachment: fixed!important;
+					  /* This is what makes the background image rescale based on its container's size */
+					  background-size: cover!important;
+					  /* Pick a solid background color that will be displayed while the background image is loading */
+					  background-color:#464646!important;
+				}
+			</style>
+	<?php
+}
+
+public function wp_add_menu() {
+//add_submenu_page('', $page_title, $menu_title, $capability, $menu_slug, $function);
+add_submenu_page('themes.php', 'Responsive Full Background Image', 'Responsive Full Background Image', 'edit_theme_options', 'wpchandra-responsive-full-background-image',  array( $this, 'admin_settings_page' ));
+add_action( 'admin_init',array($this,'register_wp_settings'));
+}
+
+
+}
+$wp_plugin_template = new WP_Chandra();
+
+}
